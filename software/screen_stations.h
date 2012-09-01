ac38261dc6e3992664843e37697bd39fb8b81d41
michael.schwarz91@gmail.com <michael.schwarz91@gmail.com@3c21ddca-31b6-481f-9578-1bbe74300639>
2012-09-01 10:16:04 +0000

    firmware, simulator: added support for 1-4 button

--- software/screen_stations.h
+++ software/screen_stations.h
@@ -4,7 +4,69 @@ typedef struct {
   char* name;
   char* genre;
   char* url;
+  char tag;
 } StationInfo;
+
+
+// ---------------------------------------------------------------------------
+void playStation(StationInfo* station) {
+  char buffer[512];
+  sprintf(buffer, "%s \"%s\" &", Settings_Get("programs", "stream"), station->url);
+  system(buffer);
+  FILE* f = fopen(Settings_Get("files", "current_station"), "w");
+  fprintf(f, "%s\r\n", station->name);
+  fclose(f);
+}
+
+// ---------------------------------------------------------------------------
+ArrayList* readStations() {
+  char buffer[256];
+  char* ptr;
+  
+  FILE* f = fopen(STATIONS_FILE, "r");
+  if(f == NULL) return NULL;
+
+  ArrayList* stations = AList_Create();
+  
+  while(fgets(buffer, 256, f) != NULL) {
+      if(strlen(buffer) < 3) continue;
+      
+      StationInfo* info = (StationInfo*)malloc(sizeof(StationInfo));
+      ptr = strtok(buffer, "|");
+      info->name = (char*)malloc(sizeof(char) * (1 + strlen(ptr)));
+      strcpy(info->name, ptr);
+      ptr = strtok(NULL, "|");
+      info->url = (char*)malloc(sizeof(char) * (1 + strlen(ptr)));
+      strcpy(info->url, ptr);
+      ptr = strtok(NULL, "|");
+      info->genre = (char*)malloc(sizeof(char) * (1 + strlen(ptr)));
+      strcpy(info->genre, ptr);
+      ptr = strtok(NULL, "|");
+      info->tag = '\0';
+      if(ptr != NULL) info->tag = ptr[0];
+      
+      AList_Add(stations, info);
+  }
+  fclose(f);
+  
+  return stations;
+}
+
+// ---------------------------------------------------------------------------
+void playFavorite(int id) {
+ ArrayList* stations = readStations();
+ 
+ int i;
+ for(i = 0; i < AList_Length(stations); i++) {
+   StationInfo* info = (StationInfo*)AList_Get(stations, i);
+   if(info->tag == id + '0') {
+    playStation(info);
+    break;
+   }
+ }
+ 
+ AList_Destroy(stations);
+}
 
 // ---------------------------------------------------------------------------
 void init_Stations() {
@@ -13,6 +75,7 @@ void init_Stations() {
 	Menu_SetAutoIO(menu_stations, 1);
 	Menu_SetBorder(menu_stations, BORDER_NONE);
 	
+	/*
 	char buffer[256];
 	char* ptr;
 	FILE* f = fopen(STATIONS_FILE, "r");
@@ -41,6 +104,17 @@ void init_Stations() {
 		if(tag != '\0' && tag != '0') Menu_SetTitleTag(menu_stations, s_id, tag);
 	}
 	fclose(f);
+	*/
+	
+	ArrayList* stations = readStations();
+	int i;
+	for(i = 0; i < AList_Length(stations); i++) {
+	  int s_id = Menu_AddItem(menu_stations, ((StationInfo*)AList_Get(stations, i))->name);
+	  Menu_AddItemTag(menu_stations, s_id, AList_Get(stations, i));
+	  char tag = ((StationInfo*)AList_Get(stations, i))->tag;
+	  if(tag != '\0' && tag != '0') Menu_SetTitleTag(menu_stations, s_id, tag);
+	}
+	AList_Destroy(stations);
 }
 
 // ---------------------------------------------------------------------------
