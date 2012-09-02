532dcb03ff13109898d86c06fe9be6ca859c8217
michael.schwarz91@gmail.com <michael.schwarz91@gmail.com@3c21ddca-31b6-481f-9578-1bbe74300639>
2012-09-02 08:21:17 +0000

    installer: added installation of required software

--- installer.sh
+++ installer.sh
@@ -8,40 +8,135 @@ echo "|  Copyright (C) 2012 by Michael Schwarz                       |"
 echo "|                                                              |"
 echo "+--------------------------------------------------------------+"
 
-# delete old installation
-sudo rm /home/pi/firmware
+# user must be root
+if [[ ${EUID} -ne 0 ]]; then
+	echo "This script must be run as root"
+	exit 1
+fi
+
+# install required software
+if [ ! -f /usr/bin/avahi-publish ];
+then
+  echo
+  echo "Installing avahi-daemon to access webradio as \"raspberrypi.local\""
+  echo
+  apt-get install avahi-daemon
+fi
+
+if [ ! -f /usr/bin/mpg123 ];
+then
+  echo
+  echo "Installing mpg123"
+  echo
+  apt-get install mpg123
+fi
+
+if [ ! -f /usr/bin/id3v2 ];
+then
+  echo
+  echo "Installing id3v2"
+  echo
+  apt-get install id3v2
+fi
+
+if [ ! -f /usr/bin/usbmount ];
+then
+  echo 
+  echo "Installing usbmount"
+  echo
+  apt-get install usbmount
+fi
+
+if [ ! -f /usr/bin/lighttpd ];
+then
+  echo
+  echo "Installing lighthttpd + php5"
+  echo
+  apt-get install lighttpd php5-cgi php5-mysql php5-curl php5-gd  php5-cgi php5-cli php5-common 
+  echo
+  echo "Create webserver directory"
+  echo
+  mkdir /var/www 2> /dev/null
+  chown pi:pi /var/www
+# TODO: change server settings
+fi
+
+if [ ! -f /usr/local/lib/libwiringPi.a ];
+then
+  echo
+  echo "Installing wiringPi"
+  echo
+  cd tools/wiringPi/wiringPi
+  make clean 2>&1 > /dev/null
+  make 2>&1 > /dev/null
+  make install
+  cd ../../..
+fi
+
+# copy mobile page
+echo
+echo "Copy mobile page to webserver directory"
+echo 
+cp -R mobile_page/* /var/www/ 2> /dev/null
+
+
+# delete old firmware
+echo 
+echo "Delete old firmware"
+echo
+rm /home/pi/firmware 2> /dev/null
 
 # build songcontrol
+echo
+echo "Build songcontrol"
+echo
 cd interfaces/songcontrol
-make clean
-make
-
+make clean 2>&1 > /dev/null
+make 2>&1 > /dev/null
+cd ../..
 
 # build firmware
-cd ../../software
-make clean
-make
+echo
+echo "Build firmware"
+echo
+cd software
+make clean 2>&1 > /dev/null
+make 2>&1 > /dev/null
+cd ..
 
 # copy file to home folder
-cd ..
-cp interfaces/songcontrol/*.sh /home/pi/
-cp interfaces/songcontrol/usb_play /home/pi/
-cp interfaces/songcontrol/stream /home/pi/
-cp software/firmware /home/pi/
+echo
+echo "Install firmware and songcontrol"
+echo
+cp interfaces/songcontrol/*.sh /home/pi/ 2> /dev/null
+cp interfaces/songcontrol/usb_play /home/pi/ 2> /dev/null
+cp interfaces/songcontrol/stream /home/pi/ 2> /dev/null
+cp software/firmware /home/pi/ 2> /dev/null
 
 # set root rights to firmware
-sudo chown root /home/pi/firmware
-sudo chmod 4755 /home/pi/firmware
+echo
+echo "Set rights for firmware"
+echo
+chown root /home/pi/firmware
+chmod 4755 /home/pi/firmware
 
 # copy language files
-mkdir /home/pi/language
-cp software/language/*.lng /home/pi/language/
+echo
+echo "Install language files"
+echo
+mkdir /home/pi/language 2> /dev/null
+cp software/language/*.lng /home/pi/language/ 2> /dev/null
 
 # copy default config
-cp software/default.conf /home/pi/
+echo
+echo "Write default configuration file"
+echo
+cp software/default.conf /home/pi/ 2> /dev/null
 
 # done
 echo "----------------------------------------------------------------"
 echo "  Installation completed"
+echo
+echo "  To start, type \"cd /home/pi; ./firmware\""
 echo "----------------------------------------------------------------"
 
